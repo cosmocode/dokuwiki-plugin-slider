f4a29ffe3508c2318444c92e68b72ee408ffc2c9
Mark Prins <mprins@users.sf.net>
2013-09-02 11:35:38 +0200

    translation update

---
+++ lang/nl/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Mark Prins <mprins@users.sf.net>
+ */
+$lang['width']                 = 'Breedte van geladen afbeeldingen. Afbeeldingen zullen worden gedownload in deze afmeting en door de browser worden geschaald.';
