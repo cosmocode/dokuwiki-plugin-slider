dd840ae15991dbbc1d0290031fc6d888eef13820
Hideaki SAWADA <chuno@live.jp>
2013-10-13 09:05:04 +0200

    translation update

---
+++ lang/ja/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['width']                 = '読み込む画像の幅。画像は、このサイズでダウンロードされ、ブラウザ内で拡大・縮小されます。';
