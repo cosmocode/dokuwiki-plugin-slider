525982c46a690b4ee9738dff387f216010257831
Myeongjin <aranet100@gmail.com>
2013-12-09 06:00:18 +0100

    translation update

---
+++ lang/ko/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['width']                 = '불러온 그림의 너비. 그림은 이 크기로 다운로드되고 브라우저에 의해 업 또는 다운스케일됩니다.';
