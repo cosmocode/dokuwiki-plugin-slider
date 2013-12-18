35ded0f46d5013efe8161003fe390ead8d276a40
Nicolas Friedli <nicolas@theologique.ch>
2013-12-18 10:25:04 +0100

    translation update

---
+++ lang/fr/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Nicolas Friedli <nicolas@theologique.ch>
+ */
+$lang['width']                 = 'Largeur des images téléchargées. Les images seront téléchargées à cette taille et seront mises à l\'échelle (plus grandes ou plus petites) par le navigateur.';
