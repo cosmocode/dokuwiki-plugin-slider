5eaaff8c801283f374559a0d1c0f90816ec25c4a
İlker R. Kapaç <irifat@gmail.com>
2015-06-04 11:25:04 +0200

    translation update

---
+++ lang/tr/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author İlker R. Kapaç <irifat@gmail.com>
+ */
+$lang['width']                 = 'Yüklenen görsellerin genişliği. Görseller ziyeretçilere bu genişlikte gönderilecek ve internet gezgini tarafından ölçeklenecektir.';
