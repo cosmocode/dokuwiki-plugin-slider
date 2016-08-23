4211bb9872bf68d9b31864035d415065b378698e
Sam01 <m.sajad079@gmail.com>
2016-08-24 00:15:30 +0200

    translation update

---
+++ lang/fa/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Sam01 <m.sajad079@gmail.com>
+ */
+$lang['width']                 = 'عرض تصاویر بارگیری شده. تصاویر دانلود می‌شوند در این اندازه و بالاتر و یا ریزمقیاس‌نمایی توسط مرورگر.';
